14c87d74a3e53d75f00047e89f5cefab84a888c5
08817711624aiml-coder <08817711624_aiml@vipstc.edu.in>
2026-06-29 16:09:51 +0530

    Time: 0 ms (100%), Space: 15 MB (44.97%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,32 +11,30 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        if(!head) return NULL;
-        vector<int>arr1;
+        if (!head)
+            return NULL;
+
+        ListNode largeDummy(0);
+        ListNode smallDummy(0);
+        ListNode* large = &largeDummy;
+        ListNode* small = &smallDummy;
+
         ListNode* temp = head;
-        while(temp){
-            arr1.push_back(temp->val);
-            temp = temp -> next;
-        }
-        vector<int>arr2;
-        int n = arr1.size();
-        int j=0;
-        for(int i =0;i<n;i++){
-            if(arr1[i] < x){
-                arr2.push_back(arr1[i]);j++;
+
+        while (temp) {
+            ListNode* nextNode = temp->next;
+            temp->next=NULL;
+            if (temp->val >= x) {
+                large->next = temp;
+                large = large->next;
             }
+            else{
+                small->next = temp;
+                small = small->next;
+            }
+            temp = nextNode;
         } 
-        for(int i = 0;i< n;i++){
-            if(arr1[i] >=x){
-                arr2.push_back(arr1[i]);
-            }
-        }
-         head = new ListNode(arr2[0]);
-        ListNode* curr= head;
-        for(int i = 1; i< arr2.size();i++){
-            curr->next = new ListNode(arr2[i]);
-            curr=curr->next;
-        }
-        return head;
+        small->next=largeDummy.next;
+        return smallDummy.next;
     }
 };

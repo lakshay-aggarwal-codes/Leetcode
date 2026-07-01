e8f18efb933c9b097839727444e088e777c7a234
08817711624aiml-coder <08817711624_aiml@vipstc.edu.in>
2026-07-01 19:51:55 +0530

    Time: 9 ms (89.06%), Space: 34.6 MB (93.49%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
- 
+
         queue<pair<int, int>> q;
         vector<vector<int>> dist(n, vector<int>(m, -1));
 
@@ -16,26 +16,25 @@ public:
             }
         }
 
+        vector<int> dr = {-1, 1, 0, 0};
+        vector<int> dc = {0, 0, -1, 1};
+
         while (!q.empty()) {
             auto curr = q.front();
             q.pop();
             int r = curr.first;
             int c = curr.second;
 
-            vector<int> dr = {-1, 1, 0, 0};
-            vector<int> dc = {0, 0, -1, 1};
-
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
-                int nc = c + dc[i]; 
+                int nc = c + dc[i];
 
                 if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
-                     
-                    if(dist[nr][nc] == -1){
-                        dist[nr][nc] = dist[r][c] + 1;
-                        q.push({nr,nc});
-                    }
 
+                    if (dist[nr][nc] == -1) {
+                        dist[nr][nc] = dist[r][c] + 1;
+                        q.push({nr, nc});
+                    }
                 }
             }
         }

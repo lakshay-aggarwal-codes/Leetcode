6c661c9bc06b08ac59a7e7654ec9589ba40f37be
Lakshay Aggarwal <lakshay.aggarwal.codes@gmail.com>
2026-08-15 21:23:51 +0530

    Time: 27 ms (33.55%), Space: 107.2 MB (53.72%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -8,10 +8,7 @@ public:
             while (st.size() > 0 && temperatures[st.top()] < temperatures[i]) {
                 ans[st.top()] = i - st.top();
                 st.pop();
-            }
-            if (st.empty()) {
-                ans[i] = 0;
-            }
+            } 
             st.push(i);
         }
         return ans;
